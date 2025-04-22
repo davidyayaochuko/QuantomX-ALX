c5d98a44c21be6d8ed64d53efda1999231ad154b
scodeq65 <sodiqagbaraojo@gmail.com>
2025-04-22 04:49:41 +0100

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 // src/App.tsx
 import { Routes, Route, Navigate } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
 import RegistrationPage from './pages/RegistrationPage';
 import StructurePage from './pages/StructurePage';
+import BrandingPage from './pages/BrandingPage';
+import InvitePage from './pages/InvitePage';
+import WorkspaceConfigurationPage from './pages/WorkspaceConfigurationPage';
+
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/register" replace />} />
-      <Route path="/register" element={<RegistrationPage />} />
-      <Route path="/structure" element={<StructurePage />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Navigate to="/register" replace />} />
+        <Route path="/register" element={<RegistrationPage />} />
+        <Route path="/structure" element={<StructurePage />} />
+        <Route path="/workspace-config" element={<WorkspaceConfigurationPage />} />
+        <Route path="/branding" element={<BrandingPage />} />
+        <Route path="/invites" element={<InvitePage />} />
+      </Routes>
+    </Layout>
   );
 }
 
